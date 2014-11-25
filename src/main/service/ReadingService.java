6fceef58394a74fb2fc8a2e5c827c7b40732c52d
Raj Bharath <rbrajbharath1@gmail.com>
2014-11-26 00:25:00 +0530

    Added Test Cases For ReadingService

--- src/main/service/ReadingService.java
+++ src/main/service/ReadingService.java
@@ -19,8 +19,13 @@ public class ReadingService {
     }
 
     public boolean borrowBook(User user, Book book) throws Exception {
+        if (user == null) throw new Exception("Null User Found");
+        if (user.getUsername() == null || user.getUsername().trim().length() < 1)
+            throw new Exception("User should have a valid username");
+        if (book == null) throw new Exception("Null Book Found");
         if (!book.isAvailable()) throw new Exception("Book Not available");
         if (!user.isAuthorized(Permission.BORROW_BOOK)) throw new Exception("User not authorized to borrow book");
+        if (book.getName() == null || book.getName().trim().length() < 1) throw new Exception("Book should have name");
 
         Reading reading = new Reading(user, book, new Date(System.currentTimeMillis()));
         book.increaseIssuedCountByOne();
@@ -28,9 +33,14 @@ public class ReadingService {
     }
 
     public boolean returnBook(User user, Book book) throws Exception {
+        if (user == null) throw new Exception("Null User Found");
+        if (user.getUsername() == null || user.getUsername().trim().length() < 1)
+            throw new Exception("User should have a valid username");
         if (!user.isAuthorized(Permission.RETURN_BOOK)) throw new Exception("User not authorized to return book");
         Reading reading = readingRepo.findByUserAndBook(user, book);
         if (reading == null) throw new Exception("User currently has no reading on the given book");
+        if (book == null) throw new Exception("Null Book Found");
+        if (book.getName() == null || book.getName().trim().length() < 1) throw new Exception("Book should have name");
         book.decreaseIssuedCountByOne();
         reading.returnReading();
         return readingRepo.update(reading);
--- src/test/service/ReadingServiceTest.java
+++ src/test/service/ReadingServiceTest.java
@@ -36,7 +36,7 @@ public class ReadingServiceTest {
     }
 
     @Test
-    public void shouldBorrowBook() throws Exception {
+    public void shouldBorrowBookWithValidUserAndBook() throws Exception {
         when(user.getUsername()).thenReturn("rajbharath");
         when(book.getName()).thenReturn("P EAA");
         when(book.isAvailable()).thenReturn(true);
@@ -51,8 +51,74 @@ public class ReadingServiceTest {
         verify(user).isAuthorized(Permission.BORROW_BOOK);
     }
 
+    @Test(expected = Exception.class)
+    public void shouldThrowExceptionUserIsNull() throws Exception {
+        when(user.getUsername()).thenReturn("rajbharath");
+        when(book.getName()).thenReturn("P EAA");
+        when(book.isAvailable()).thenReturn(true);
+        when(user.isAuthorized(Permission.BORROW_BOOK)).thenReturn(true);
+        when(readingRepo.save(any(Reading.class))).thenReturn(true);
+        ReadingService service = new ReadingService(readingRepo);
+        service.borrowBook(null, book);
+    }
+
+    @Test(expected = Exception.class)
+    public void shouldThrowExceptionForBookNotAvailable() throws Exception {
+        when(user.getUsername()).thenReturn("rajbharath");
+        when(book.getName()).thenReturn("P EAA");
+        when(book.isAvailable()).thenReturn(false);
+        when(user.isAuthorized(Permission.BORROW_BOOK)).thenReturn(true);
+        when(readingRepo.save(any(Reading.class))).thenReturn(true);
+        ReadingService service = new ReadingService(readingRepo);
+        service.borrowBook(user, book);
+    }
+
+    @Test(expected = Exception.class)
+    public void shouldThrowExceptionToBorrowBookForUnauthorizedUser() throws Exception {
+        when(user.getUsername()).thenReturn("rajbharath");
+        when(book.getName()).thenReturn("P EAA");
+        when(book.isAvailable()).thenReturn(true);
+        when(user.isAuthorized(Permission.BORROW_BOOK)).thenReturn(false);
+        when(readingRepo.save(any(Reading.class))).thenReturn(true);
+        ReadingService service = new ReadingService(readingRepo);
+        service.borrowBook(user, book);
+    }
+
+    @Test(expected = Exception.class)
+    public void shouldThrowExceptionToBorrowBookWithBookNameSpaces() throws Exception {
+        when(user.getUsername()).thenReturn("rajbharath");
+        when(book.getName()).thenReturn("   ");
+        when(book.isAvailable()).thenReturn(true);
+        when(user.isAuthorized(Permission.BORROW_BOOK)).thenReturn(true);
+        when(readingRepo.save(any(Reading.class))).thenReturn(true);
+        ReadingService service = new ReadingService(readingRepo);
+
+        service.borrowBook(user, book);
+
+    }
+
+    @Test(expected = Exception.class)
+    public void shouldThrowExceptionToBorrowBookWithUserNameSpaces() throws Exception {
+        when(user.getUsername()).thenReturn("    ");
+        when(book.getName()).thenReturn("P EAA");
+        when(book.isAvailable()).thenReturn(true);
+        when(user.isAuthorized(Permission.BORROW_BOOK)).thenReturn(true);
+        when(readingRepo.save(any(Reading.class))).thenReturn(true);
+        ReadingService service = new ReadingService(readingRepo);
+
+        service.borrowBook(user, book);
+
+    }
+
+    @Test(expected = Exception.class)
+    public void shouldThrowExceptionBothUserAndBookIsNull() throws Exception {
+        ReadingService service = new ReadingService(readingRepo);
+        service.borrowBook(null, null);
+
+    }
+
     @Test
-    public void shouldReturnBook() throws Exception {
+    public void shouldReturnValidBookForValidUser() throws Exception {
         when(user.getUsername()).thenReturn("rajbharath").thenReturn("rajbharath").thenReturn("rajbharath");
         when(book.getName()).thenReturn("P EAA").thenReturn("P EAA").thenReturn("P EAA");
         when(user.isAuthorized(Permission.RETURN_BOOK)).thenReturn(true);
@@ -65,7 +131,77 @@ public class ReadingServiceTest {
         verify(user).isAuthorized(Permission.RETURN_BOOK);
         verify(book).decreaseIssuedCountByOne();
         verify(reading).returnReading();
+    }
 
+    @Test(expected = Exception.class)
+    public void shouldThrowExceptionToReturnBookWithUserIsNull() throws Exception {
+        when(book.getName()).thenReturn("P EAA").thenReturn("P EAA").thenReturn("P EAA");
+        when(readingRepo.findByUserAndBook(user, book)).thenReturn(reading);
+        when(readingRepo.update(any(Reading.class))).thenReturn(true);
+        ReadingService service = new ReadingService(readingRepo);
+        service.returnBook(null, book);
+    }
+
+    @Test(expected = Exception.class)
+    public void shouldThrowExceptionToReturnBookForUnauthorizedUser() throws Exception {
+        when(user.getUsername()).thenReturn("rajbharath").thenReturn("rajbharath").thenReturn("rajbharath");
+        when(book.getName()).thenReturn("P EAA").thenReturn("P EAA").thenReturn("P EAA");
+        when(user.isAuthorized(Permission.RETURN_BOOK)).thenReturn(false);
+        when(readingRepo.findByUserAndBook(user, book)).thenReturn(reading);
+        when(readingRepo.update(any(Reading.class))).thenReturn(true);
+        ReadingService service = new ReadingService(readingRepo);
+
+        assertTrue("should return book failed", service.returnBook(user, book));
+
+        verify(user).isAuthorized(Permission.RETURN_BOOK);
+        verify(book).decreaseIssuedCountByOne();
+        verify(reading).returnReading();
+    }
+
+    @Test(expected = Exception.class)
+    public void shouldThrowExceptionToReturnBookWithBookNameSpaces() throws Exception {
+        when(user.getUsername()).thenReturn("rajbharath").thenReturn("rajbharath").thenReturn("rajbharath");
+        when(book.getName()).thenReturn(" ").thenReturn(" ").thenReturn("  ");
+        when(user.isAuthorized(Permission.RETURN_BOOK)).thenReturn(true);
+        when(readingRepo.findByUserAndBook(user, book)).thenReturn(reading);
+        when(readingRepo.update(any(Reading.class))).thenReturn(true);
+        ReadingService service = new ReadingService(readingRepo);
+
+        service.returnBook(user, book);
+    }
+
+    @Test(expected = Exception.class)
+    public void shouldThrowExceptionToReturnBookWithUserNameSpaces() throws Exception {
+        when(user.getUsername()).thenReturn("    ").thenReturn("   ").thenReturn("   ");
+        when(book.getName()).thenReturn("P EAA").thenReturn("P EAA").thenReturn("P EAA");
+        when(user.isAuthorized(Permission.RETURN_BOOK)).thenReturn(true);
+        when(readingRepo.findByUserAndBook(user, book)).thenReturn(reading);
+        when(readingRepo.update(any(Reading.class))).thenReturn(true);
+        ReadingService service = new ReadingService(readingRepo);
+
+        service.returnBook(user, book);
+    }
+
+    @Test(expected = Exception.class)
+    public void shouldThrowExceptionToReturnBookBothUserAndBookIsNull() throws Exception {
+        ReadingService service = new ReadingService(readingRepo);
+        service.returnBook(null, null);
+    }
+
+    @Test
+    public void sample() throws Exception {
+        when(user.getUsername()).thenReturn("rajbharath").thenReturn("rajbharath").thenReturn("rajbharath");
+        when(book.getName()).thenReturn("P EAA").thenReturn("P EAA").thenReturn("P EAA");
+        when(user.isAuthorized(Permission.RETURN_BOOK)).thenReturn(true);
+        when(readingRepo.findByUserAndBook(user, book)).thenReturn(reading);
+        when(readingRepo.update(any(Reading.class))).thenReturn(true);
+        ReadingService service = new ReadingService(readingRepo);
+
+        assertTrue("should return book failed", service.returnBook(user, book));
+
+        verify(user).isAuthorized(Permission.RETURN_BOOK);
+        verify(book).decreaseIssuedCountByOne();
+        verify(reading).returnReading();
     }
 
 }
